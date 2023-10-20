43b2444be25160a15fb9bdfc549ffef99e95fbbc
AhmadHussain921 <ahmad.hussain@techilab.com>
2023-10-20 10:00:06 +0500

    changed flow of schema and added admin middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,6 @@ const { register, login, allUsers } = require("../controller/user");
 router.get("/", allUsers);
 router.post("/register", register);
 router.post("/login", login);
+
+//export your routes
 module.exports = router;
